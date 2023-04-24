27a3baf172b22ccabd28e8246ad57eff1f430f49
Son Dang <minhson0506@gmail.com>
2023-04-24 16:05:22 +0300

    add test user function

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose"
-import {postUser} from "./userFunction"
+import {deleteUser, getSingleUser, getUsers, login, loginBrute, postUser, putUser} from "./userFunction"
 import app from "../src/app"
 import {UserTest} from "../src/interfaces/User"
 import randomstring from "randomstring"
+import LoginMessageResponse from "../src/interfaces/LoginMessageResponse"
+import {getNotFound} from "./testFunction"
 
 describe('GET /graphql', () => {
     beforeAll(async () => {
@@ -13,15 +15,71 @@ describe('GET /graphql', () => {
         await mongoose.connection.close()
     })
 
+    // test not found
+    it('responds with a not found message', async () => {
+        await getNotFound(app);
+    });
+
     const testUser: UserTest = {
         user_name: 'Test User ' + randomstring.generate(7),
         email: randomstring.generate(9) + '@user.fi',
         password: 'testpassword',
-      };
+    };
+
+    let userData: LoginMessageResponse;
 
     // test user
     // create user
     it('should create a user', async () => {
         await postUser(app, testUser)
     })
-})
+
+    // login user
+    it('should login a user', async () => {
+        userData = await login(app, testUser)
+    })
+
+    // test get all users
+    it('should return array of users', async () => {
+        await getUsers(app);
+    });
+
+    // test get single user
+    it('should return single user', async () => {
+        await getSingleUser(app, userData.user.id!);
+    });
+
+    // test update user
+    it('should update user', async () => {
+        await putUser(app, userData.token!);
+    });
+
+    it('should delete current user', async () => {
+        await deleteUser(app, userData.token!);
+    });
+
+    // test brute force protectiom
+    test('Brute force attack simulation', async () => {
+        const maxAttempts = 20;
+        const mockUser: UserTest = {
+            user_name: 'Test User ' + randomstring.generate(7),
+            email: randomstring.generate(9) + '@user.fi',
+            password: 'notthepassword',
+        };
+
+        try {
+            // Call the mock login function until the maximum number of attempts is reached
+            for (let i = 0; i < maxAttempts; i++) {
+                const result = await loginBrute(app, mockUser);
+                if (result) throw new Error('Brute force attack unsuccessful');
+            }
+
+            // If the while loop completes successfully, the test fails
+            throw new Error('Brute force attack succeeded');
+        } catch (error) {
+            console.log(error);
+            // If the login function throws an error, the test passes
+            expect((error as Error).message).toBe('Brute force attack unsuccessful');
+        }
+    }, 15000);
+});
--- test/userFunction.ts
+++ test/userFunction.ts
@@ -1,6 +1,9 @@
 import request from 'supertest';
 import expect from 'expect';
 import {UserTest} from "../src/interfaces/User";
+import LoginMessageResponse from '../src/interfaces/LoginMessageResponse';
+import ErrorResponse from '../src/interfaces/ErrorResponse';
+import randomstring from 'randomstring';
 
 // create users
 const postUser = async (url: string | Function, user: UserTest): Promise<UserTest> => {
@@ -39,8 +42,224 @@ const postUser = async (url: string | Function, user: UserTest): Promise<UserTes
                 expect(userData.user.email).toBe(user.email);
                 resolve(response.body.data.register);
             }
-        );
+            );
     });
 };
 
-export {postUser};
+const getUsers = async (url: string | Function): Promise<UserTest[]> => {
+    return new Promise((resolve, reject) => {
+        request(url)
+            .post('/graphql')
+            .set('Content-Type', 'application/json')
+            .send({
+                query: `query Query {
+                    users {
+                        id
+                        user_name
+                        email
+                    }
+                }`,
+            })
+            .expect(200, (err, response) => {
+                if (err) {
+                    reject(err);
+                }
+                const users = response.body.data.users;
+                expect(users).toBeInstanceOf(Array);
+                expect(users[0]).toHaveProperty('id');
+                expect(users[0]).toHaveProperty('user_name');
+                expect(users[0]).toHaveProperty('email');
+                resolve(users);
+            }
+            );
+    });
+};
+
+const getSingleUser = async (url: string | Function, id: string): Promise<UserTest> => {
+    return new Promise((resolve, reject) => {
+        request(url)
+            .post('/graphql')
+            .set('Content-Type', 'application/json')
+            .send({
+                query: `query Query($userByIdId: ID!) {
+                    userById(id: $userByIdId) {
+                        id
+                        user_name
+                        email
+                    }
+                }`,
+                variables: {
+                    userByIdId: id,
+                },
+            })
+            .expect(200, (err, response) => {
+                if (err) {
+                    reject(err);
+                }
+                const user = response.body.data.userById;
+                expect(user).toHaveProperty('id');
+                expect(user).toHaveProperty('user_name');
+                expect(user).toHaveProperty('email');
+                resolve(user);
+            }
+            );
+    });
+};
+
+const login = async (url: string | Function, user: UserTest): Promise<LoginMessageResponse> => {
+    return new Promise((resolve, reject) => {
+        request(url)
+            .post('/graphql')
+            .set('Content-Type', 'application/json')
+            .send({
+                query: `mutation Mutation($credentials: Credentials!) {
+                    login(credentials: $credentials) {
+                        message
+                        token
+                        user {
+                            id
+                            user_name
+                            email
+                        }
+                    }
+                }`,
+                variables: {
+                    credentials: {
+                        username: user.email,
+                        password: user.password,
+                    },
+                },
+            })
+            .expect(200, (err, response) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    const loginData = response.body.data.login;
+                    expect(loginData).toHaveProperty('message');
+                    expect(loginData).toHaveProperty('token');
+                    expect(loginData).toHaveProperty('user');
+                    expect(loginData.user).toHaveProperty('id');
+                    expect(loginData.user.user_name).toBe(user.user_name);
+                    expect(loginData.user.email).toBe(user.email);
+                    resolve(response.body.data.login);
+                }
+            }
+            );
+    });
+};
+
+const loginBrute = async (url: string | Function, user: UserTest): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+        request(url)
+            .post('/graphql')
+            .set('Content-Type', 'application/json')
+            .send({
+                query: `mutation Mutation($credentials: Credentials!) {
+                    login(credentials: $credentials) {
+                        message
+                        token
+                        user {
+                            id
+                            user_name
+                            email
+                        }
+                    }
+                }`,
+                variables: {
+                    credentials: {
+                        username: user.email,
+                        password: user.password,
+                    },
+                },
+            })
+            .expect(200, (err, response) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    if (
+                        response.body.errors?.[0]?.message ===
+                        "You are trying to access 'login' too often"
+                    ) {
+                        console.log('brute blocked', response.body.errors[0].message);
+                        resolve(true);
+                    } else {
+                        resolve(false);
+                    }
+                }
+            });
+    });
+};
+
+const putUser = (url: string | Function, token: string) => {
+    return new Promise((resolve, reject) => {
+        const newValue = 'Test Loser ' + randomstring.generate(7);
+        request(url)
+            .post('/graphql')
+            .set('Content-type', 'application/json')
+            .set('Authorization', 'Bearer ' + token)
+            .send({
+                query: `mutation UpdateUser($user: UserModify!) {
+                    updateUser(user: $user) {
+                        message
+                        user {
+                            id
+                            user_name
+                            email
+                        }
+                    }
+                }`,
+                variables: {
+                    user: {
+                        user_name: newValue,
+                    },
+                },
+            })
+            .expect(200, (err, response) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    const userData = response.body.data.updateUser;
+                    expect(userData).toHaveProperty('message');
+                    expect(userData).toHaveProperty('user');
+                    expect(userData.user).toHaveProperty('id');
+                    expect(userData.user.user_name).toBe(newValue);
+                    resolve(response.body.data.updateUser);
+                }
+            });
+    });
+};
+
+const deleteUser = (
+    url: string | Function,
+    token: string
+): Promise<ErrorResponse> => {
+    return new Promise((resolve, reject) => {
+        request(url)
+            .post('/graphql')
+            .set('Authorization', 'Bearer ' + token)
+            .send({
+                query: `mutation DeleteUser {
+                    deleteUser {
+                        message
+                        user {
+                            id
+                            user_name
+                            email
+                        }
+                    }
+                }`,
+            })
+            .expect(200, (err, response) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    const userData = response.body.data.deleteUser;
+                    expect(userData).toHaveProperty('message');
+                    expect(userData).toHaveProperty('user');
+                    resolve(response.body.data.deleteUser);
+                }
+            });
+    });
+};
+
+export {postUser, getUsers, getSingleUser, login, loginBrute, putUser, deleteUser};
